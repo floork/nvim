88be256611836fedce6d52d0894b85d3ee65c660
floork <mros.florian@gmail.com>
2022-10-19 18:58:15 +0200

    my nvim configs

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# install the dependencies
+if [ -x "$(command -v pacman)" ]
+then
+	sudo pacman -Syu
+	sudo pacman -S --noconfirm --needed ctags nodejs npm
+elif [ -x "$(command -v dnf)" ]
+then
+    sudo -S dnf -y install ctags nodejs npm
+elif [ -x "$(command -v apt-get)" ]
+then
+    sudo apt-get update && sudo apt-get upgrade
+    sudo apt-get -y --yes install exuberant-ctags nodejs npm
+else
+    echo 'This Distro is not supported!'
+fi
+
+# copy the configs to config location
+if [ -d "~/.config/nvim" ]; then
+	curl -s https://raw.githubusercontent.com/floork/nvim/main/init.vim > ~/.config/nvim/init.vim
+else
+    mkdir ~/.config/nvim
+	curl -s https://raw.githubusercontent.com/floork/nvim/main/init.vim > ~/.config/nvim/init.vim
+fi
--- readme.md
+++ readme.md
@@ -1,3 +1,4 @@
 # just run 
 ```sh
+curl -s https://raw.githubusercontent.com/floork/my_shell/main/install.sh > testing.sh
 ```
