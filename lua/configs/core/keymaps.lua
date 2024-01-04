2d217977b47b1e64e03c6e364e296d22fdb8c004
Florian Mros <florian_alexander.mros@tu-dresden.de>
2024-01-04 17:23:49 +0100

    add(keymap): added a keymap for indenting in visualmode

--- lua/configs/core/keymaps.lua
+++ lua/configs/core/keymaps.lua
@@ -10,7 +10,9 @@ keymap.set("n", "x", '"_x', { desc = "Delete without yanking", noremap = true, s
 
 keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move line down", noremap = true, silent = true })
 keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move line up", noremap = true, silent = true })
-keymap.set("x", "<C-v>", [[:normal I<Tab><Esc>]], { noremap = true, silent = true })
+
+keymap.set("x", "<C-t>", ">gv", { desc = "Indent right", noremap = true, silent = true })
+keymap.set("x", "<C-d>", "<gv", { desc = "Indent left", noremap = true, silent = true })
 
 -- Search and replace
 keymap.set(
