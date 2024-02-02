f696f2ccd8239c3e15999d8be5c0adcdd8fa4339
Florian Mros <florian_alexander.mros@tu-dresden.de>
2024-02-02 07:55:13 +0100

    add: harpoon2

--- lua/configs/plugins/harpoon.lua
+++ lua/configs/plugins/harpoon.lua
@@ -1,14 +1,13 @@
 return {
   "ThePrimeagen/harpoon",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-  },
+  branch = "harpoon2",
+  dependencies = { "nvim-lua/plenary.nvim" },
   config = function()
     local keymap = vim.keymap
-    local mark = require("harpoon.mark")
+    local harpoon = require("harpoon")
     local ui = require("harpoon.ui")
 
-    keymap.set("n", "<leader>hm", mark.add_file, { desc = "Mark file with harpoon" })
+    keymap.set("n", "<leader>a", function() harpoon:list():append() end)
     keymap.set("n", "<leader>hn", ui.nav_next, { desc = "Go to next harpoon mark" })
     keymap.set("n", "<leader>hp", ui.nav_prev, { desc = "Go to previous harpoon mark" })
     keymap.set("n", "<leader>hh", ui.toggle_quick_menu, { desc = "Toggle harpoon quick menu" })
