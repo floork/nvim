ca7c709c1fd88c52b508d7e312295bf2773553ae
Florian Mros <florian_alexander.mros@tu-dresden.de>
2024-02-02 08:19:46 +0100

    fix: harpoon keymaps

--- lua/configs/plugins/harpoon.lua
+++ lua/configs/plugins/harpoon.lua
@@ -3,13 +3,14 @@ return {
   branch = "harpoon2",
   dependencies = { "nvim-lua/plenary.nvim" },
   config = function()
-    local keymap = vim.keymap
     local harpoon = require("harpoon")
-    local ui = require("harpoon.ui")
 
-    keymap.set("n", "<leader>a", function() harpoon:list():append() end)
-    keymap.set("n", "<leader>hn", ui.nav_next, { desc = "Go to next harpoon mark" })
-    keymap.set("n", "<leader>hp", ui.nav_prev, { desc = "Go to previous harpoon mark" })
-    keymap.set("n", "<leader>hh", ui.toggle_quick_menu, { desc = "Toggle harpoon quick menu" })
+    harpoon:setup({})
+
+    local keymap = vim.keymap
+    keymap.set("n", "<leader>hm", function() harpoon:list():append() end)
+    keymap.set("n", "<leader>hl", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+    keymap.set("n", "<leader>hp", function() harpoon:list():prev() end)
+    keymap.set("n", "<leader>hn", function() harpoon:list():next() end)
   end,
 }
