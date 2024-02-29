f142870fc528db5752241da88019ce956b069da0
Florian Mros <florian_alexander.mros@tu-dresden.de>
2024-02-29 15:58:33 +0100

    add(codeium): chat integration + other accept key

--- lua/configs/plugins/codeium.lua
+++ lua/configs/plugins/codeium.lua
@@ -1,4 +1,19 @@
 return {
   'Exafunction/codeium.vim',
-  event = 'BufEnter'
+  event = 'BufEnter',
+  config = function()
+    vim.g.codeium_disable_bindings = 1
+
+    -- keymap
+    local keymap = vim.keymap
+    local opts = { noremap = true, silent = true }
+
+    opts.desc = "Codeium Accept"
+    keymap.set('i', '<C-l>',
+      function() return vim.fn.feedkeys(vim.api.nvim_replace_termcodes(vim.fn["codeium#Accept"](), true, true, true), "") end,
+      opts)
+    keymap.set('n', '<leader>/',
+      function() return vim.fn['codeium#Chat']() end,
+      opts)
+  end
 }
