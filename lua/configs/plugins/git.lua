d0bb243e51dc209453654aa344a05372b6ffb578
Florian Mros <florian_alexander.mros@tu-dresden.de>
2024-03-17 18:47:32 +0100

    add(git): using neogit instead of fugitive for now

--- lua/configs/plugins/git.lua
+++ lua/configs/plugins/git.lua
@@ -30,36 +30,78 @@ return {
       keymap.set("n", "<leader>gm", "<cmd>Telescope git_worktree create_git_worktree<cr>", opts)
     end,
   },
+  -- {
+  --   "tpope/vim-fugitive",
+  --   config = function()
+  --     local keymap = vim.keymap
+  --     local opts = { noremap = true, silent = true }
+  --
+  --     opts.desc = "Git status"
+  --     keymap.set("n", "<leader>gs", "<cmd>Git<CR>", opts)
+  --     opts.desc = "Git push"
+  --     keymap.set("n", "<leader>gp", function()
+  --       vim.cmd.Git("push")
+  --     end, opts)
+  --     opts.desc = "Git push --force-with-lease"
+  --     keymap.set("n", "<leader>gfp", function()
+  --       vim.cmd.Git("push --force-with-lease")
+  --     end, opts)
+  --     opts.desc = "Git push --force"
+  --     keymap.set("n", "<leader>gFp", function()
+  --       vim.cmd.Git("push --force")
+  --     end, opts)
+  --     opts.desc = "Git pull --rebase"
+  --     keymap.set("n", "<leader>gP", function()
+  --       vim.cmd.Git("pull --rebase")
+  --     end, opts)
+  --     opts.desc = "Git push origin"
+  --     keymap.set("n", "<leader>go", ":Git push -u origin ", opts)
+  --     opts.desc = "Git blame"
+  --     keymap.set("n", "<leader>gb", ":Git blame<CR>", opts)
+  --     opts.desc = "Git log"
+  --     keymap.set("n", "<leader>gl", ":Git log<CR>", opts)
+  --   end,
+  -- },
   {
-    "tpope/vim-fugitive",
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "sindrets/diffview.nvim",
+      "nvim-telescope/telescope.nvim",
+      "ibhagwan/fzf-lua",
+    },
     config = function()
+      local neogit = require("neogit")
+
+      neogit.setup({
+        integrations = {
+          diffview = true,
+        },
+        disable_prompt_on_change = true,
+        kind = "split",
+        commit_view = {
+          kind = "replace",
+        },
+        commit_select_view = {
+          kind = "split",
+        },
+      })
+
+      -- keymap
       local keymap = vim.keymap
       local opts = { noremap = true, silent = true }
 
-      opts.desc = "Git status"
-      keymap.set("n", "<leader>gs", "<cmd>Git<CR>", opts)
-      opts.desc = "Git push"
-      keymap.set("n", "<leader>gp", function()
-        vim.cmd.Git("push")
-      end, opts)
-      opts.desc = "Git push --force-with-lease"
-      keymap.set("n", "<leader>gfp", function()
-        vim.cmd.Git("push --force-with-lease")
-      end, opts)
-      opts.desc = "Git push --force"
-      keymap.set("n", "<leader>gFp", function()
-        vim.cmd.Git("push --force")
-      end, opts)
-      opts.desc = "Git pull --rebase"
-      keymap.set("n", "<leader>gP", function()
-        vim.cmd.Git("pull --rebase")
-      end, opts)
-      opts.desc = "Git push origin"
-      keymap.set("n", "<leader>go", ":Git push -u origin ", opts)
-      opts.desc = "Git blame"
-      keymap.set("n", "<leader>gb", ":Git blame<CR>", opts)
-      opts.desc = "Git log"
-      keymap.set("n", "<leader>gl", ":Git log<CR>", opts)
+      opts.desc = "Neogit"
+      keymap.set("n", "<leader>gs", "<cmd>Neogit<CR>", opts)
+
+      opts.desc = "Neogit log"
+      keymap.set("n", "<leader>gl", "<cmd>Neogit log<CR>", opts)
+
+      opts.desc = "Neogit push"
+      keymap.set("n", "<leader>gp", "<cmd>Neogit push<CR>", opts)
+
+      opts.desc = "Neogit pull"
+      keymap.set("n", "<leader>gP", "<cmd>Neogit pull<CR>", opts)
     end,
-  },
+  }
 }
