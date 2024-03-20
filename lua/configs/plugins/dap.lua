bb1fe5e0f20079233848ee8d09738a71e8b402c1
Florian Mros <florian_alexander.mros@tu-dresden.de>
2024-03-20 14:13:16 +0100

    fix(dap): better options for cpp

--- lua/configs/plugins/dap.lua
+++ lua/configs/plugins/dap.lua
@@ -112,35 +112,6 @@ return {
       dapui.close({})
     end
 
-    local api = vim.api
-    local keymap_restore = {}
-    dap.listeners.after['event_initialized']['me'] = function()
-      for _, buf in pairs(api.nvim_list_bufs()) do
-        local keymaps = api.nvim_buf_get_keymap(buf, 'n')
-        for _, keymap in pairs(keymaps) do
-          if keymap.lhs == "K" then
-            table.insert(keymap_restore, keymap)
-            api.nvim_buf_del_keymap(buf, 'n', 'K')
-          end
-        end
-      end
-      api.nvim_set_keymap(
-        'n', 'U', '<Cmd>lua require("dap.ui.widgets").hover()<CR>', { silent = true })
-    end
-
-    dap.listeners.after['event_terminated']['me'] = function()
-      for _, keymap in pairs(keymap_restore) do
-        api.nvim_buf_set_keymap(
-          keymap.buffer,
-          keymap.mode,
-          keymap.lhs,
-          keymap.rhs,
-          { silent = keymap.silent == 1 }
-        )
-      end
-      keymap_restore = {}
-    end
-
     dap.adapters.gdb = {
       type = "executable",
       command = "gdb",
@@ -155,7 +126,7 @@ return {
         program = function()
           vim.api.nvim_command('!cmake --build build')
 
-          local executable_path = vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+          local executable_path = vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/build/', 'file')
           return executable_path
         end,
         cwd = "${workspaceFolder}",
