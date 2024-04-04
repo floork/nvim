7330d28ff87a2e38dd6a92189d0b096233da11a6
Florian Mros <florian_alexander.mros@tu-dresden.de>
2024-04-04 11:38:33 +0200

    add(term): local or remote

--- lua/configs/plugins/term.lua
+++ lua/configs/plugins/term.lua
@@ -1,8 +1,31 @@
-return {
-  "floork/term.nvim",
-  config = function()
-    require("term").setup({
-      toggle_terminal_key = "<C-t>"
-    })
+local function get_term_plugins(include_local_term)
+  local plugins = {
+    {
+      "floork/term.nvim",
+      config = function()
+        require("term").setup({
+          toggle_terminal_key = "<C-t>"
+        })
+      end
+    }
+  }
+
+  if include_local_term then
+    local term_nvim_path = "~/GIT/GitHub/term.nvim"
+    if vim.fn.isdirectory(term_nvim_path) == 1 then
+      table.insert(plugins, {
+        "term.nvim",
+        dir = term_nvim_path,
+        config = function()
+          require("term").setup({
+            toggle_terminal_key = "<C-t>"
+          })
+        end
+      })
+    end
   end
-}
+
+  return plugins
+end
+
+return get_term_plugins(false)
