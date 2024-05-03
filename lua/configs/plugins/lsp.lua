d6b21b460a15409c9f42ad6feb0e3725c98dcac3
Florian Mros <florian_alexander.mros@tu-dresden.de>
2024-05-03 12:58:21 +0200

    fix: moved all autocmd in right place

--- lua/configs/plugins/lsp.lua
+++ lua/configs/plugins/lsp.lua
@@ -14,40 +14,6 @@ return {
       { "j-hui/fidget.nvim", opts = {} },
     },
     config = function()
-      vim.api.nvim_create_autocmd("LspAttach", {
-        group = vim.api.nvim_create_augroup("configs-lsp-attach", { clear = true }),
-        callback = function(event)
-          local map = function(keys, func, desc)
-            vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
-          end
-
-          map("gR", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-          map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-          map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
-
-          map("gb", "<cmd> pop<CR>", "[G]oto [B]uffer")
-
-          map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
-          map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
-          map("<leader>Ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
-          map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
-          map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
-          map("K", vim.lsp.buf.hover, "Hover Documentation")
-          local client = vim.lsp.get_client_by_id(event.data.client_id)
-          if client and client.server_capabilities.documentHighlightProvider then
-            vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-              buffer = event.buf,
-              callback = vim.lsp.buf.document_highlight,
-            })
-
-            vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-              buffer = event.buf,
-              callback = vim.lsp.buf.clear_references,
-            })
-          end
-        end,
-      })
-
       -- used to enable autocompletion (assign to every lsp server config)
       local capabilities = vim.lsp.protocol.make_client_capabilities()
       capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
