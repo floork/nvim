14f98224a8272f5500fd762844b13010e2383bdd
Florian Mros <florian_alexander.mros@tu-dresden.de>
2024-06-11 13:49:46 +0200

    chore(codeium): deactivated
    not used currently

--- lua/configs/plugins/codeium.lua
+++ lua/configs/plugins/codeium.lua
@@ -1,12 +1,14 @@
 return {
-  "Exafunction/codeium.nvim",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "hrsh7th/nvim-cmp",
-  },
-  config = function()
-    require("codeium").setup({
-      enable_chat = true,
-    })
-  end
+  -- currently not used
+
+  -- "Exafunction/codeium.nvim",
+  -- dependencies = {
+  --   "nvim-lua/plenary.nvim",
+  --   "hrsh7th/nvim-cmp",
+  -- },
+  -- config = function()
+  --   require("codeium").setup({
+  --     enable_chat = true,
+  --   })
+  -- end
 }
