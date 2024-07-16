b2ae0b604414bcb9a8736fc15adf9fd930dbf387
Florian Mros <florian_alexander.mros@tu-dresden.de>
2024-07-16 14:27:30 +0200

    fix: undotree

--- lua/configs/plugins/undotree.lua
+++ lua/configs/plugins/undotree.lua
@@ -1,10 +1,8 @@
 return {
   "jiaoshijie/undotree",
   dependencies = "nvim-lua/plenary.nvim",
-  config = function()
-    local keymap = vim.keymap
-    local opts = { noremap = true, silent = true }
-    opts.desc = "Toggle undotree"
-    keymap.set("n", "<leader>u", "<CMD>UndotreeToggle<cr>", opts)
-  end,
+  config = true,
+  keys = { -- load the plugin only when using it's keybinding:
+    { "<leader>u", "<cmd>lua require('undotree').toggle()<cr>", { noremap = true, silent = true, desc = "Toggle undotree" } },
+  },
 }
