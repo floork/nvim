63a9f6bc2b7dffde3d2a435a29860a691108cd04
Florian Mros <florian_alexander.mros@tu-dresden.de>
2024-08-22 13:48:46 +0200

    add(conform): toggle autoformat

--- lua/configs/plugins/formatting.lua
+++ lua/configs/plugins/formatting.lua
@@ -38,11 +38,17 @@ return {
         toml = { "taplo" },                                       -- TOML formatter (prettier)
         yml = { "prettier" },                                     -- YAML formatter (prettier)
       },
-      format_on_save = {
-        lsp_fallback = true,
-        async = false,
-        timeout_ms = 1000,
-      },
+      format_on_save = function(bufnr)
+        -- Disable with a global or buffer-local variable
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
+        return {
+          lsp_fallback = true,
+          async = false,
+          timeout_ms = 1000,
+        }
+      end,
     })
 
     conform.formatters.my_cpp = {
@@ -50,5 +56,23 @@ return {
       args = { "-style=Google" },
       stdin = true,
     }
+
+    vim.api.nvim_create_user_command("FormatDisable", function(args)
+      if args.bang then
+        -- FormatDisable! will disable formatting just for this buffer
+        vim.b.disable_autoformat = true
+      else
+        vim.g.disable_autoformat = true
+      end
+    end, {
+      desc = "Disable autoformat-on-save",
+      bang = true,
+    })
+    vim.api.nvim_create_user_command("FormatEnable", function()
+      vim.b.disable_autoformat = false
+      vim.g.disable_autoformat = false
+    end, {
+      desc = "Re-enable autoformat-on-save",
+    })
   end,
 }
