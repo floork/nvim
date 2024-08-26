43a9fbf35e8ff0e864a6eefcbf09ae87277c903a
Florian Mros <florian_alexander.mros@tu-dresden.de>
2024-08-26 12:10:31 +0200

    cleanup test

--- lua/configs/plugins/trouble.lua
+++ lua/configs/plugins/trouble.lua
@@ -1,13 +1,14 @@
 return {
-  "folke/trouble.nvim",
-  keys = {
-    {
-      "<leader>xx",
-      function()
-        require("trouble").toggle("diagnostics")
-      end,
-      desc = "Toggle trouble"
-    },
-  },
-  config = true,
+  -- testing if i really need it
+  -- "folke/trouble.nvim",
+  -- keys = {
+  --   {
+  --     "<leader>xx",
+  --     function()
+  --       require("trouble").toggle("diagnostics")
+  --     end,
+  --     desc = "Toggle trouble"
+  --   },
+  -- },
+  -- config = true,
 }
