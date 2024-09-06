fc04242e141718bfc74c5c65d31896f6e86abb8c
Florian Mros <florian_alexander.mros@tu-dresden.de>
2024-09-06 15:33:18 +0200

    fix(lsp): tsserver support

--- lua/configs/plugins/lsp.lua
+++ lua/configs/plugins/lsp.lua
@@ -131,6 +131,9 @@ return {
       require("mason-lspconfig").setup({
         handlers = {
           function(server_name)
+            if server_name == "tsserver" then
+              server_name = "ts_ls"
+            end
             local server = servers[server_name] or {}
             server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
             require("lspconfig")[server_name].setup(server)
