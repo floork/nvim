e673e5378a46cd3732dea17797792dbc5bacea6d
Florian Mros <florian_alexander.mros@tu-dresden.de>
2024-10-10 09:55:32 +0200

    change(keymaps): more uniform keymaps

--- lua/configs/core/keymaps.lua
+++ lua/configs/core/keymaps.lua
@@ -4,29 +4,43 @@ vim.g.maplocalleader = " "
 
 -- Define a local variable for keymap utility
 local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
 
 -- Text operations
-keymap.set("n", "x", '"_x', { desc = "Delete without yanking", noremap = true, silent = true })
-keymap.set("v", "d", '"_x', { desc = "Delete without yanking", noremap = true, silent = true })
+opts.desc = "Delete without yanking single char"
+keymap.set("n", "x", '"_x', opts)
+opts.desc = "Delete without yanking selected"
+keymap.set("v", "d", '"_x', opts)
+opts.desc = "Delete without yanking to end of line"
+keymap.set("n", "D", '"_D', opts)
+opts.desc = "Normal pasting"
+keymap.set("i", "<C-v>", '<C-r>+', opts)
 
 -- Search and replace
+opts.desc = "Search and replace"
 keymap.set(
   "n",
   "<leader>s",
   [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
-  { desc = "Search and replace", noremap = true, silent = true }
+  opts
 )
 
 -- Move Lines UP and DOWN
-keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move line down", noremap = true, silent = true })
-keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move line up", noremap = true, silent = true })
+opts.desc = "Move line down"
+keymap.set("v", "J", ":m '>+1<CR>gv=gv", opts)
+opts.desc = "Move line up"
+keymap.set("v", "K", ":m '<-2<CR>gv=gv", opts)
 
 -- Join lines without moving cursor
-keymap.set("n", "J", "mzJ`z", { desc = "Join lines without moving cursor", noremap = true, silent = true })
-keymap.set("n", "U", "i<Enter><Esc>", { desc = "Split lines at cursor", noremap = true })
+opts.desc = "Join lines without moving cursor"
+keymap.set("n", "J", "mzJ`z", opts)
+opts.desc = "Split lines at cursor"
+keymap.set("n", "U", "i<Enter><Esc>", opts)
 
 -- Toggle file explorer
-keymap.set("n", "<leader>ee", vim.cmd.Ex, { desc = "Toggle file explorer", noremap = true, silent = true })
+opts.desc = "Toggle file explorer"
+keymap.set("n", "<leader>ee", vim.cmd.Ex, opts)
 
 -- Readjust the Window size
-keymap.set("n", "<leader>ww", "<cmd>wincmd =<CR>", { desc = "Readjust the window size", noremap = true, silent = true })
+opts.desc = "Readjust the window size"
+keymap.set("n", "<leader>ww", "<cmd>wincmd =<CR>", opts)
