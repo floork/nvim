bfaf57a9c7086fb1d93f80b6c02469f36f8a577e
Florian Mros <florian_alexander.mros@tu-dresden.de>
2025-02-19 10:53:01 +0100

    fix(color): window seperator

--- lua/configs/plugins/color.lua
+++ lua/configs/plugins/color.lua
@@ -10,6 +10,7 @@ function ColorMyPencil(color)
   vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
   vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
   vim.api.nvim_set_hl(0, "DiffChange", { bg = "none" })
+  vim.api.nvim_set_hl(0, "WinSeparator", { bg = "none" })
   vim.api.nvim_set_hl(0, "ColorColumn", { bg = "#3c3836" })
 end
 
