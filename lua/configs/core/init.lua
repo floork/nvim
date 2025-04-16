b08f557b3fc4d39df116090b0ad5e7a9d71c2f75
Florian Mros <florian_alexander.mros@tu-dresden.de>
2025-04-16 13:43:30 +0200

    fix: netrw bindings

--- lua/configs/core/init.lua
+++ lua/configs/core/init.lua
@@ -2,14 +2,8 @@ require("configs.core.keymaps")
 require("configs.core.options")
 require("configs.core.autocommands")
 require("configs.core.usercommands")
+require("configs.core.netrw")
 require("configs.core.custom")
-
--- netrw
-vim.g.netrw_banner = 0
-vim.g.netrw_winsize = 15
-vim.g.netrw_liststyle = 3
-vim.g.netrw_browse_split = 0
-vim.g.netrw_list_depth = 999
 
 vim.filetype.add({
   filename = {
---
+++ lua/configs/core/netrw.lua
@@ -0,0 +1,17 @@
+-- netrw
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 15
+vim.g.netrw_liststyle = 3
+vim.g.netrw_browse_split = 0
+vim.g.netrw_list_depth = 999
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "netrw",
+  callback = function()
+    -- Unmap Ctrl+l
+    vim.keymap.set("n", "<C-l>", '<Cmd>lua navigate("l")<CR>', { buffer = true })
+
+    -- Remap Ctrl+r to reload the netrw directory view
+    vim.keymap.set("n", "<C-r>", "<Plug>NetrwRefresh", { buffer = true, silent = true })
+  end,
+})
