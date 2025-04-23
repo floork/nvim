848fc52d8b6b40aa66550b82b8792f95dedbf64d
Florian Mros <florian_alexander.mros@tu-dresden.de>
2025-04-23 14:01:33 +0200

    fix(pyright): settings

--- lsp/pyright.lua
+++ lsp/pyright.lua
@@ -1,4 +1,20 @@
 return {
-  cmd = { "pyright" },
+  cmd = { "pyright-langserver", "--stdio" },
+  root_markers = {
+    ".git",               -- Common Git marker
+    "pyproject.toml",     -- Modern Python packaging standard
+    "setup.py",           -- Older but common packaging script
+    "setup.cfg",          -- Configuration for setuptools
+    "requirements.txt",   -- Common location for dependencies list
+    "pyrightconfig.json", -- Pyright's specific configuration file
+  },
   filetypes = { "python" },
+  settings = {
+    python = {
+      analysis = {
+        useLibraryCodeForTypes = true,
+        typeCheckingMode = "standard",
+      },
+    },
+  },
 }
