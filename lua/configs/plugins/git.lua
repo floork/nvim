735c35ec6fbad31cad9c9b73e6fdf6a1687dcd49
Florian Mros <florian_alexander.mros@tu-dresden.de>
2025-04-23 14:10:48 +0200

    feat: tring out nvim withou lazygit

--- lua/configs/plugins/git.lua
+++ lua/configs/plugins/git.lua
@@ -21,22 +21,22 @@ return {
     end,
   },
   {
-    "NeogitOrg/neogit",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-    keys = {
-      { "<leader>gs", "<cmd>Neogit<CR>",      desc = "Neogit" },
-      { "<leader>gl", "<cmd>Neogit log<CR>",  desc = "Neogit log" },
-      { "<leader>gp", "<cmd>Neogit push<CR>", desc = "Neogit push" },
-      { "<leader>gP", "<cmd>Neogit pull<CR>", desc = "Neogit pull" }
-    },
-    config = function()
-      local neogit = require("neogit")
-
-      neogit.setup({
-        disable_prompt_on_change = true,
-      })
-    end,
+    -- "NeogitOrg/neogit",
+    -- dependencies = {
+    --   "nvim-lua/plenary.nvim",
+    -- },
+    -- keys = {
+    --   { "<leader>gs", "<cmd>Neogit<CR>",      desc = "Neogit" },
+    --   { "<leader>gl", "<cmd>Neogit log<CR>",  desc = "Neogit log" },
+    --   { "<leader>gp", "<cmd>Neogit push<CR>", desc = "Neogit push" },
+    --   { "<leader>gP", "<cmd>Neogit pull<CR>", desc = "Neogit pull" }
+    -- },
+    -- config = function()
+    --   local neogit = require("neogit")
+    --
+    --   neogit.setup({
+    --     disable_prompt_on_change = true,
+    --   })
+    -- end,
   }
 }
