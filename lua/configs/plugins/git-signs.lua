97e0a96e5ecd53b9f85a292bcefcec6e814a0515
Florian Mros <florian_alexander.mros@tu-dresden.de>
2025-05-09 14:22:34 +0200

    rename: git->git-signs

---
+++ lua/configs/plugins/git-signs.lua
@@ -0,0 +1,21 @@
+return {
+    "lewis6991/gitsigns.nvim",
+    opts = {
+      signs = {
+        add = { text = "+" },
+        change = { text = "~" },
+        delete = { text = "_" },
+        topdelete = { text = "‾" },
+        changedelete = { text = "~" },
+      },
+    },
+    keys = {
+      { "<leader>gb", "<CMD>Gitsigns blame<cr>", desc = "Git blame" }
+    },
+    lazy = false,
+    config = function(_, opts)
+      local gs = require("gitsigns")
+
+      gs.setup(opts)
+    end,
+}
