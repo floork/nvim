6bd511da7ec5c68389d5b0de70837438028e5471
Florian Mros <florian_alexander.mros@tu-dresden.de>
2025-05-17 15:12:48 +0200

    fix(conform, lsp): use ruff + fix stylua

---
+++ lsp/ruff.lua
@@ -0,0 +1,12 @@
+return {
+  cmd = { "ruff", "server" },
+  root_markers = {
+    ".git", -- Common Git marker
+    "pyproject.toml", -- Modern Python packaging standard
+    "setup.py", -- Older but common packaging script
+    "setup.cfg", -- Configuration for setuptools
+    "requirements.txt", -- Common location for dependencies list
+    "pyrightconfig.json", -- Pyright's specific configuration file
+  },
+  filetypes = { "python" },
+}
--- lua/configs/plugins/conform.lua
+++ lua/configs/plugins/conform.lua
@@ -1,9 +1,7 @@
 -- Lua function to check if a `.clang-format` file exists
 local function clang_format_command()
   local handle = io.popen('test -f .clang-format && echo "found" || echo "not found"')
-  if handle == nil then
-    return { "-style=llvm" }
-  end
+  if handle == nil then return { "-style=llvm" } end
   local result = handle:read("*a"):match("^%s*(.-)%s*$")
   handle:close()
   if result == "found" then
@@ -42,32 +40,30 @@ return {
 
     conform.setup({
       formatters_by_ft = {
-        bash = { "shfmt" },                                       -- Bash formatter (shfmt)
-        sh = { "shfmt" },                                         -- Shell formatter (shfmt)
-        zsh = { "shfmt" },                                        -- Zsh formatter (shfmt)
-        c = { "my_cpp" },                                         -- C formatter (clang-format)
-        cpp = { "my_cpp" },                                       -- C++ formatter (clang-format)
-        css = { "prettier" },                                     -- CSS formatter (prettier)
-        go = { "gofmt" },                                         -- Go formatter (gofmt)
-        html = { "prettier" },                                    -- HTML formatter (prettier)
-        javascript = { "prettier" },                              -- JavaScript formatter (prettier)
-        json = { "prettier" },                                    -- JSON formatter (prettier)
-        jsonc = { "prettier" },                                   -- JSON formatter (prettier)
-        lua = { "stylua --indent-width 2 --indent-type Spaces" }, -- Lua formatter (stylua)
-        markdown = { "prettier" },                                -- Markdown formatter (prettier)
-        nix = { "nixfmt" },                                       -- Nix formatter (nixfmt)
-        python = { "isort", "black" },                            -- Python formatters (isort, black)
-        rust = { "rust_analyzer" },                               -- Rust formatter (rustfmt)
-        tex = { "tex_fmt" },                                      -- LaTeX formatter (tex-fmt)
-        toml = { "taplo" },                                       -- TOML formatter (prettier)
-        yml = { "prettier" },                                     -- YAML formatter (prettier)
+        bash = { "shfmt" }, -- Bash formatter (shfmt)
+        sh = { "shfmt" }, -- Shell formatter (shfmt)
+        zsh = { "shfmt" }, -- Zsh formatter (shfmt)
+        c = { "my_cpp" }, -- C formatter (clang-format)
+        cpp = { "my_cpp" }, -- C++ formatter (clang-format)
+        css = { "prettier" }, -- CSS formatter (prettier)
+        go = { "gofmt" }, -- Go formatter (gofmt)
+        html = { "prettier" }, -- HTML formatter (prettier)
+        javascript = { "prettier" }, -- JavaScript formatter (prettier)
+        json = { "prettier" }, -- JSON formatter (prettier)
+        jsonc = { "prettier" }, -- JSON formatter (prettier)
+        lua = { "stylua" }, -- Lua formatter (stylua)
+        markdown = { "prettier" }, -- Markdown formatter (prettier)
+        nix = { "nixfmt" }, -- Nix formatter (nixfmt)
+        python = { "ruff" }, -- Python formatters
+        rust = { "rust_analyzer" }, -- Rust formatter (rustfmt)
+        tex = { "tex_fmt" }, -- LaTeX formatter (tex-fmt)
+        toml = { "taplo" }, -- TOML formatter (prettier)
+        yml = { "prettier" }, -- YAML formatter (prettier)
       },
 
       format_on_save = function(bufnr)
         -- Disable with a global or buffer-local variable
-        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-          return
-        end
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then return end
 
         return {
           lsp_fallback = true,
