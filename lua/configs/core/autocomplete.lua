db27739efc2908b9871eaf92736ab3aef2b31ace
Florian Mros <florian_alexander.mros@tu-dresden.de>
2025-06-02 14:49:54 +0200

    fix(autocomplete): eror on completionItem not supported

--- lua/configs/core/autocomplete.lua
+++ lua/configs/core/autocomplete.lua
@@ -8,9 +8,6 @@ vim.api.nvim_create_autocmd("LspAttach", {
     local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
 
     -- Set trigger characters for autocompletion
-    -- Note: This will only work if the server supports the dynamic registration
-    -- of trigger characters or if the trigger characters are initially empty.
-    -- Some servers might not allow overriding this after initialization.
     if client.server_capabilities.completionProvider then
       client.server_capabilities.completionProvider.triggerCharacters =
         vim.split("qwertyuiopasdfghjklzxcvbnm.", "")
@@ -18,7 +15,6 @@ vim.api.nvim_create_autocmd("LspAttach", {
 
     -- Check if the client supports the textDocument/completion method
     if not client:supports_method("textDocument/completion") then
-      -- If the client does not support completion, show a notification
       vim.notify(
         "LSP server for " .. client.name .. " does not support textDocument/completion.",
         vim.log.levels.INFO
@@ -29,33 +25,45 @@ vim.api.nvim_create_autocmd("LspAttach", {
       autotrigger = true,
     })
 
-    -- Resolve completion item documentation on selection change
-    local _, cancel_prev = nil, function() end
-    vim.api.nvim_create_autocmd("CompleteChanged", {
-      buffer = args.buf,
-      callback = function()
-        cancel_prev()
-        local info = vim.fn.complete_info({ "selected" })
-        local completionItem =
-          vim.tbl_get(vim.v.completed_item, "user_data", "nvim", "lsp", "completion_item")
-        if completionItem == nil then return end
-        _, cancel_prev = vim.lsp.buf_request(
-          args.buf,
-          vim.lsp.protocol.Methods.completionItem_resolve,
-          completionItem,
-          function(err, item, ctx)
-            if not item then return end
-            local docs = (item.documentation or {}).value
-            local win = vim.api.nvim__complete_set(info["selected"], {
-              info = docs,
-            })
-            if win.winid and vim.api.nvim_win_is_valid(win.winid) then
-              vim.treesitter.start(win.bufnr, "markdown")
-              vim.wo[win.winid].conceallevel = 3
+    -- Only set up completion item resolution if the server supports it
+    if client:supports_method("completionItem/resolve") then
+      local _, cancel_prev = nil, function() end
+      vim.api.nvim_create_autocmd("CompleteChanged", {
+        buffer = args.buf,
+        callback = function()
+          cancel_prev()
+          local info = vim.fn.complete_info({ "selected" })
+          local completionItem =
+            vim.tbl_get(vim.v.completed_item, "user_data", "nvim", "lsp", "completion_item")
+          if completionItem == nil then return end
+
+          _, cancel_prev = vim.lsp.buf_request(
+            args.buf,
+            vim.lsp.protocol.Methods.completionItem_resolve,
+            completionItem,
+            function(err, item, ctx)
+              if err or not item then return end
+              local docs = (item.documentation or {}).value
+              if not docs then return end
+              local win = vim.api.nvim__complete_set(info["selected"], {
+                info = docs,
+              })
+              if win.winid and vim.api.nvim_win_is_valid(win.winid) then
+                vim.treesitter.start(win.bufnr, "markdown")
+                vim.wo[win.winid].conceallevel = 3
+              end
             end
-          end
-        )
-      end,
-    })
+          )
+        end,
+      })
+    else
+      vim.notify(
+        "LSP server for "
+          .. client.name
+          .. " does not support completionItem/resolve. "
+          .. "Documentation preview will not be available.",
+        vim.log.levels.DEBUG
+      )
+    end
   end,
 })
